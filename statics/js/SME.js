5f1d4eff04afa1819570631a3f7926d2fac79dde
samynv <d.diks@mail.blastedsolutions.com>
2015-05-21 22:06:40 +0200

    Made searching work on FF

--- statics/js/SME.js
+++ statics/js/SME.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    $('#form-field-search').keypress(function(e){
        // This is enter
-       if(e.charCode === 13)
+       if(e.charCode === 13 || e.key === "Enter")
        {
            var text = e.currentTarget.value;
            text = text.replace(' ', '+');

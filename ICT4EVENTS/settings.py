47ed56ca482cb5bc0672f23e997b6455b0db700c
Requinard <d.diks@mail.blastedsolutions.com>
2015-05-26 13:09:05 +0200

    Updated databases

--- ICT4EVENTS/settings.py
+++ ICT4EVENTS/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'ICT4EVENTS.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'sqlite': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
-    'default':{
+    'pstgres':{
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'pts2',
         'USER': 'david',

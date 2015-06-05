0e22eb8ab1066730bb4dfed17baf270242f14c25
Requinard <d.diks@mail.blastedsolutions.com>
2015-06-05 12:37:57 +0200

    stuff

--- events/tests.py
+++ events/tests.py
@@ -4,5 +4,9 @@ from django.utils.timezone import datetime
 from .models import *
 
 # Create your tests here.
+class PlekMethodReserveTests(TestCase):
+    def setUp(self):
+        pass
 
-
+    def test_create_reservation(self):
+        pass

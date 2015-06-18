8fbbb4edbf23d0d1661499292a7a2d9e92545c24
David Diks <contact@terarion.com>
2015-06-18 21:16:52 +0200

    Reworked reservation urls

--- reservations/urls.py
+++ reservations/urls.py
@@ -3,9 +3,9 @@ from reservations.views import IndexView, CartView, CartConfirmView, CartDeleteV
 
 urlpatterns = patterns(
     "",
-    url(r'^addtocart/(?P<product_id>\d+)/', CartView.as_view(), name="addtocart"),
-    url(r'^confirmcart/',CartConfirmView.as_view(), name="confirmcart"),
-    url(r'^deletecart/',CartDeleteView.as_view(), name="deletecart"),
+    url(r'^cart/add/(?P<product_id>\d+)/', CartView.as_view(), name="addtocart"),
+    url(r'^cart/confirm/',CartConfirmView.as_view(), name="confirmcart"),
+    url(r'^cart/delete/',CartDeleteView.as_view(), name="deletecart"),
     url(r'^place/new/(?P<place_id>\d+)/', PlaceAddnewPerson.as_view(), name="place_add"),
     url(r'^place/', PlaceReservationView.as_view(), name="place"),
     url(r"^$", IndexView.as_view(), name="index"),

9fdae3a629d45b51bab1029585d7f2e9081d3dc2
Requinard <d.diks@mail.blastedsolutions.com>
2015-06-18 13:33:07 +0200

    ICT4EVENTS-WebApp-25 Add download links for documents

--- sharing/templatetags/file_field.py
+++ sharing/templatetags/file_field.py
@@ -1,11 +1,21 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+@register.filter
+@stringfilter
 def linkify(value):
     value = str(value)
 
     extension = value.split('.')[-1]
-
+    print(value)
     if extension in ["jpg", "jpeg", "gif", "png"]:
-        return "<img src='{}' class='img-responsive'" % value
+        return "<img src='{0}' class='img-responsive'>".format(value)
+    elif extension in ["mp3", "wav", "ogg"]:
+        print("<audio controls><source='{0}'></audio>".format(value))
+        return '<audio src="{0}" controls><a href="{0}"> Download</a></audio>'.format(value)
+    elif extension in ["webm",]:
+        return '<video src="{0}" controls class="img-responsive">you browser doesnt support embedded video. <a href="{0}>Download it here</a></video>'.format(value)
+    else:
+        return '<a href="{0}" class="btn btn-warning">Download dit bestand</a>'.format(value)

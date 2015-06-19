5cd6307e2c0c0f7385024557ce2f669d3d704e3f
Requinard <d.diks@mail.blastedsolutions.com>
2015-06-19 12:47:34 +0200

    Renamed post

--- staff/urls.py
+++ staff/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, patterns
 
-from staff.views import IndexView
+from staff.views import IndexView, GenerateView
 
 urlpatterns = patterns(
     '',
+    url(r'^generate/', GenerateView.as_view(), name="generate"),
     url(r'^$', IndexView.as_view(), name="index"),
 )
--- staff/views.py
+++ staff/views.py
@@ -1,12 +1,13 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import View
-from reservations.models import Verhuur
+from events.models import Polsbandje
+from reservations.models import Verhuur, Product, Productcat, Productexemplaar
 from staff.forms import BarcodeForm
 from accounts.models import ReserveringPolsbandje
-
+import random
 
 class IndexView(View):
     def get(self, request):
@@ -42,3 +43,46 @@ class IndexView(View):
         context['form'] = form
 
         return render(request, "staff/index.html", context)
+
+class GenerateView(View):
+    def get(self, request):
+        self.generatePolsbandje()
+        #self.generateProducts()
+        return redirect("staff:index")
+
+    def generatePolsbandje(self):
+        try:
+            for i in range(1000, 10000):
+                p = Polsbandje(barcode=i, actief=False)
+                p.save()
+        except:
+            pass
+
+    def generateProducts(self):
+        procat = Productcat.objects.get_or_create(naam="test")[0]
+        product = Product(productcategorie=procat, merk="Lenovo", serie="Y510p", typenummer="5", prijs=500)
+        product.save()
+
+        for i in range(0, 10):
+            b = random.randint(1000, 10000000)
+            pe = Productexemplaar(barcode=b, product=product)
+
+            pe.save()
+
+        product = Product(productcategorie=procat, merk="Kruidvat", serie="Tandenborstel", typenummer="Ultimate", prijs=10)
+        product.save()
+
+        for i in range(0, 10):
+            b = random.randint(1000, 10000000)
+            pe = Productexemplaar(barcode=b, product=product)
+
+            pe.save()
+
+        product = Product(productcategorie=procat, merk="Slaapzak", serie="Super Deluxe", typenummer="Ultimate", prijs=100)
+        product.save()
+
+        for i in range(0, 10):
+            b = random.randint(1000, 10000000)
+            pe = Productexemplaar(barcode=b, product=product)
+
+            pe.save()

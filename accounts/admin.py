4fce1a3f011ca636bd56e7d1c2089690e7e89b79
Requinard <d.diks@mail.blastedsolutions.com>
2015-06-22 15:21:16 +0200

    Added aanwezigheidsfilter

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import *
 
+class ReserveringsPolsbandjeAdmin(admin.ModelAdmin):
+    list_filter = ("aanwezig", "reservering__plekken__locatie")
+
 # Register your models here.
 admin.site.register(Account)
-admin.site.register(ReserveringPolsbandje)
+admin.site.register(ReserveringPolsbandje, ReserveringsPolsbandjeAdmin)

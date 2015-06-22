52be037df01e35edc56433385002d13fa7679327
Requinard <d.diks@mail.blastedsolutions.com>
2015-06-22 15:35:26 +0200

    Fixed decorator

--- ICT4EVENTS/decorators.py
+++ ICT4EVENTS/decorators.py
@@ -5,11 +5,11 @@ from django.shortcuts import redirect
 
 def event_is_active(function):
     def wrap(request, *args, **kwargs):
-
-
         if request.user.settings.active_event is None:
             messages.error(request, "Je hebt nog geen inschrijving voor een event")
-            return HttpResponseRedirect('/')
+            return redirect("events:index")
+        else:
+            return function(request, *args, **kwargs)
 
 
     wrap.__doc__=function.__doc__
